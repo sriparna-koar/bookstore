40dda6bea22658dd1f54e83c7017565192cef6ad
sriparna-koar <koarsk03@gmail.com>
2024-03-05 11:51:31 +0530

    modifying main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <SnackbarProvider>
       <App />
