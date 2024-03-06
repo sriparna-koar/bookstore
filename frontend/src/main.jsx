23aa864ada4b26de6f58e50b0f4d20bf11a4bdaa
sriparna-koar <koarsk03@gmail.com>
2024-03-06 22:28:10 +0530

    adding css and timestamp to the caht app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <SnackbarProvider>
       <App />
