c39936d4baa52380767f92e6807efd614bb95e25
sriparna-koar <koarsk03@gmail.com>
2024-03-06 22:46:28 +0530

    modifying main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
+// import React from 'react';
+// import ReactDOM from 'react-dom/client'
 
+// import App from './App.jsx';
+// import './index.css';
+// import { BrowserRouter } from 'react-router-dom';
+// import { SnackbarProvider } from 'notistack';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//     <SnackbarProvider>
+//       <App />
+//     </SnackbarProvider>
+//   </BrowserRouter>
+// );
+
+
+import React from 'react';
+import { createRoot } from 'react-dom/client'; // Import createRoot from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <SnackbarProvider>
-      <App />
-    </SnackbarProvider>
-  </BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>
 );
-
-
