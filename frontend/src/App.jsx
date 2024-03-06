67a49c4b1cfabc40bfe2cb158f80868525b1bf73
sriparna-koar <koarsk03@gmail.com>
2024-03-06 23:15:39 +0530

    modifying app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,31 @@
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import CreateBook from './pages/CreateBooks';
+// import ShowBook from './pages/ShowBook';
+// import EditBook from './pages/EditBook';
+// import DeleteBook from './pages/DeleteBook';
+// import './App.css';
+
+// const App = () => {
+//   return (
+//     <Routes>
+//       <Route path='/' element={<Home />} />
+//       <Route path='/books/create' element={<CreateBook />} />
+//       <Route path='/books/details/:id' element={<ShowBook />} />
+//       <Route path='/books/edit/:id' element={<EditBook />} />
+//       <Route path='/books/delete/:id' element={<DeleteBook />} />
+//     </Routes>
+//   );
+// };
+
+// export default App;
+
+
+
+
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'; // Change import here
 import Home from './pages/Home';
 import CreateBook from './pages/CreateBooks';
 import ShowBook from './pages/ShowBook';
@@ -9,13 +35,15 @@ import './App.css';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBook />} />
-      <Route path='/books/details/:id' element={<ShowBook />} />
-      <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/books/delete/:id' element={<DeleteBook />} />
-    </Routes>
+    <Router> {/* Use Router here */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books/create' element={<CreateBook />} />
+        <Route path='/books/details/:id' element={<ShowBook />} />
+        <Route path='/books/edit/:id' element={<EditBook />} />
+        <Route path='/books/delete/:id' element={<DeleteBook />} />
+      </Routes>
+    </Router>
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,13 +14,24 @@
 //   </BrowserRouter>
 // );
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <Router>
+// createRoot(document.getElementById('root')).render(
+//   <Router>
+//     <App />
+//   </Router>
+// );
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </Router>
-);
+  </React.StrictMode>,
+)
