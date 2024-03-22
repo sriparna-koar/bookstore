07e0f897317dfc91d5164c8b7d1d6c0d73f1c6d8
sriparna-koar <koarsk03@gmail.com>
2024-03-22 21:49:58 +0530

    firdt

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Spinner from '../components/Spinner';
+import Spinner from '../components/Spinner.jsx';
 import { Link } from 'react-router-dom';
 import './Home.css';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
